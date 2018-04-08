ac479b830f9c85a0c0ea5c181660d1d72385bc94
Nguyen Tuan <dinhtuan1991vt2013@gmail.com>
2018-04-08 14:43:34 +0700

    Add form search

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,5 +1,7 @@
 class DevelopersController < ApplicationController
   def index
+    @ruby_id = ProgrammingLanguage.find_by_name('ruby')&.id
+    @english_id = Language.find_by_code('en')&.id
     @developers = SearchDeveloperService.new(params).call
   end
 end
