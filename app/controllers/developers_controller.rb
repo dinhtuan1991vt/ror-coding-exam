40da09642d33e74a67e628304048a30844a572da
Nguyen Tuan <dinhtuan1991vt2013@gmail.com>
2018-04-08 19:35:40 +0700

    Use keyword text instead of checkbox for search

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,7 +1,6 @@
 class DevelopersController < ApplicationController
   def index
-    @ruby_id = ProgrammingLanguage.find_by_name('ruby')&.id
-    @english_id = Language.find_by_code('en')&.id
+    @keyword = params[:keyword]
     @developers = SearchDeveloperService.new(params).call
   end
 end
--- app/services/search_developer_service.rb
+++ app/services/search_developer_service.rb
@@ -1,27 +1,18 @@
 class SearchDeveloperService
-  attr_reader :programming_language_id, :language_id
+  attr_reader :keyword
 
   def initialize(params = {})
     @relation = Developer.includes(:programming_languages, :languages)
-    @programming_language_id = params[:programming_language_id].presence
-    @language_id = params[:language_id].presence
+    @keyword = params[:keyword].presence
   end
 
   def call
-    search_with_programming_language if programming_language_id
-    search_with_language if language_id
+    search_with_keyword if keyword
     @relation
   end
 
   private
 
-  def search_with_programming_language
-    @relation = @relation.left_joins(:programming_languages)
-                         .where('programming_languages.id = ?', programming_language_id)
-  end
-
-  def search_with_language
-    @relation = @relation.left_joins(:languages)
-                         .where('languages.id = ?', language_id)
+  def search_with_keyword
   end
 end
