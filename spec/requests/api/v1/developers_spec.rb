22ab9463c02f482195c4bfdbcbe2be9f1231feab
Nguyen Tuan <dinhtuan1991vt2013@gmail.com>
2018-04-08 22:50:46 +0700

    Test api

---
+++ spec/requests/api/v1/developers_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "API Developers", type: :request do
+  describe 'GET /api/v1/developers' do
+    let!(:developers) { create_list(:developer, 100) }
+
+    context 'return correct data' do
+      before { get '/api/v1/developers' }
+
+      it 'return success status' do
+        expect(response).to be_success
+      end
+
+      it 'return correct data' do
+        expect(json).not_to be_empty
+        expect(json['data'].size).to eq(100)
+      end
+    end
+  end
+end
